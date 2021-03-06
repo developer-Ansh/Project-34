2b3292f75c49e535485cd1904e832c4eb9f0b153
developer-Ansh <74807349+developer-Ansh@users.noreply.github.com>
2021-03-06 14:07:49 +0530

    Add files via upload

---
+++ Monster.js
@@ -0,0 +1,24 @@
+class  Monster{
+    constructor(x,y,r){
+    var options = {
+       friction:1,
+       density:0.5
+    }  
+        this.body = Bodies.circle(x,y,r,options);
+        this.radius = r
+        this.image = loadImage("images/Monster-01.png");
+        World.add(world, this.body);    
+
+    }
+
+  display(){
+    var angle = this.body.angle;
+    var pos = this.body.position
+    push(); 
+    translate(pos.x,pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image,0, 0, this.radius, this.radius);
+    pop();
+  }  
+};
